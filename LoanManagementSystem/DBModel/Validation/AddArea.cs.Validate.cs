be54168d5e4a64c1419cbac4b3bad6ab24a0b0e9
sbpallekumbura <sbpallekumbura@gmail.com>
2015-09-14 13:07:20 +0530

    recommit 3

---
+++ LoanManagementSystem/DBModel/Validation/AddArea.cs.Validate.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace LoanManagementSystem.DBModel
+{
+    public partial class area : IDataErrorInfo
+    {
+        public string Error
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public string this[string columnName]
+        {
+            get
+            {
+                string result = null;
+                if (columnName == "AREA_CODE")
+                {
+                    if (string.IsNullOrEmpty(AREA_CODE))
+                    {
+                        result = "Area Code cannot be empty";
+                    }
+                }
+                if (columnName == "AREA_NAME")
+                {
+                    if (String.IsNullOrWhiteSpace(AREA_NAME))
+                    {
+                        result = "Area Name cannot be empty";
+                    }
+                }                
+
+                return result;
+            }
+        }
+    }
+}
---
+++ LoanManagementSystem/DBService/Implementions/AreaService.cs
@@ -0,0 +1,83 @@
+﻿using LoanManagementSystem.DBModel;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity.Validation;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LoanManagementSystem.DBService.Implementions
+{
+    class AreaService:GenericService<area>
+    {
+        public static int InsertArea(area _area)
+        {
+            try
+            {
+                db.areas.Add(_area);
+                return db.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+                return 0;
+            }
+        }
+
+        public static int UpdateSMS(area _area)
+        {
+            try
+            {
+                var area = db.areas.Single(ar => ar.ID == _area.ID);
+                db.Entry(area).CurrentValues.SetValues(_area);
+
+                return db.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+                return 0;
+            }
+        }
+
+        public static int DeleteSMS(area _area)
+        {
+            try
+            {
+                var area = db.areas.Single(ar => ar.ID == _area.ID);
+                db.areas.Remove(area);
+                return db.SaveChanges();
+            }
+            catch (DbEntityValidationException dbEx)
+            {
+                foreach (var validationErrors in dbEx.EntityValidationErrors)
+                {
+                    foreach (var validationError in validationErrors.ValidationErrors)
+                    {
+                        Trace.TraceInformation("Property: {0} Error: {1}", validationError.PropertyName, validationError.ErrorMessage);
+                    }
+                }
+                return 0;
+            }
+        }
+
+        internal static System.Collections.IEnumerable getAreaCodes()
+        {
+            var areas = db.areas.Where(e => e.STATUS == true).ToList();
+            return areas;
+        }
+    }
+}
